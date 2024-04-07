b8355850cf8bf165afc568b58899b7ceade01627
Ruben-Bellido <bellido.ruben@opendeusto.es>
2024-04-07 17:55:46 +0200

    Añadir URLs por consola + tracking de precio cada 1min

--- app/price_tracker.py
+++ app/price_tracker.py
@@ -2,56 +2,83 @@ import requests
 from bs4 import BeautifulSoup
 from lxml import etree as et
 import time
-import random
 import csv
 
+# Cabeceras para simular una solicitud de navegador real (evita los sistemas anti tracking de Amazon)
 header = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36",
-    'Accept': '*/*', 'Accept-Encoding': 'gzip, deflate, br', 'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8'
+    'Accept': '*/*',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8'
 }
 
-bucket_list = ['https://www.amazon.es/HP-15-fd0042ns-Ordenador-port%C3%A1til-Graphics/dp/B0CFBGWVQD/ref=sr_1_4?dib=eyJ2IjoiMSJ9.aoMhGcee8VYmgN3D0U2sTAqtFmpV83m0jdH6lkzhsI6oPWLraNdPXElQ1tFkt8lard6Fj0KKREb4PzAtGwtXQJNRfN9oNldoc6N1mjYSHDWIVcA3RVW-X7JIG49CmXXsetu4R-RZw-4Ds8_vJ5VLLErHwRSxLzZMs4qkUIV7W-6h432NypNPo6cWpcaaUxum9c6-uSKtsPxbB8wX1eUB2Wq2WstpRLjoXu2YWVCUMf0v-01oAbVDA1bQKgqlN4PfBQ88kfLecQieqlcPEmLpgRdqr0IxGtHDlxA608kr6Zk.DPLrA6WevfsiJ3BGsxTOnyC4w8H1-nO2TJ-DMu2dVYY&dib_tag=se&qid=1712414913&s=computers&sr=1-4&ufe=app_do%3Aamzn1.fos.0fd54328-1d46-4534-bd0f-16141b40bb5b',
-               'https://www.amazon.es/Rii-Mini-i8-ergon%C3%B3mico-touchpad/dp/B00YDSSB6A?pf_rd_r=84P3B8YYS15RGM67DHNR&pf_rd_t=Events&pf_rd_i=deals&pf_rd_p=c2fe896f-6544-4e82-9c0f-79dcf8a574c4&pf_rd_s=slot-14&ref=dlx_deals_gd_dcl_img_11_19b3468c_dt_sl14_c4'
-               ]
+# Lista de URLs de productos en Amazon
+bucket_list = []
 
-
-def get_amazon_price(dom):
-
+# Registrar urls mediante inputs
+while True:
+    url = input('Introduce la url del producto a trackear ("fin" para salir): ')
+    # Condición de salida
+    if url == 'fin':
+        break
     try:
+        # Validar la URL mediante una solicitud HTTP
+        requests.get(url, headers=header)
+        # Añadir URL al listado
+        bucket_list.append(url)
+        print('URL añadida\n')
+    except Exception:
+        print('URL inválida\n')
+
+
+# Obtiene el precio del producto
+def get_amazon_price(dom):
+    try:
+        # Extraer el precio del producto utilizando XPath
         price = dom.xpath('//span[@class="a-offscreen"]/text()')[0]
-        #price = price.replace(',', '').replace('€', '').replace('.00', '')
-        #return int(price)
+        # Parsear el precio a formato float
         price = price.replace(',', '.').replace('€', '').replace('.00', '')
         return float(price)
-    except Exception as e:
+    except Exception:
         price = 'Not Available'
         return None
 
-
+# Obtiene el nombre del producto
 def get_product_name(dom):
     try:
+        # Extraer el nombre del producto utilizando XPath
         name = dom.xpath('//span[@id="productTitle"]/text()')
+        # Extrae el primer elemento (contiene el nombre del producto) y elimina los espacios al principio y al final
         [name.strip() for name in name]
         return name[0]
-    except Exception as e:
+    except Exception:
         name = 'Not Available'
         return None
 
-# write data into a csv file
 
-with open('master_data.csv', 'w') as csv_file:
-    writer = csv.writer(csv_file)
-    writer.writerow(['product name', 'price', 'url'])
+# Escribir datos en un archivo CSV
+while True:
+    with open('master_data.csv', 'w') as csv_file:
+        writer = csv.writer(csv_file)
+        # Escribir encabezados en la primera fila del archivo
+        writer.writerow(['product name', 'price', 'url'])
 
-    for url in bucket_list:
-        response = requests.get(url, headers=header)
-        soup = BeautifulSoup(response.content, 'html.parser')
-        amazon_dom = et.HTML(str(soup))
+        # Por cada URL del listado
+        for url in bucket_list:
+            # Realizar una solicitud HTTP a la URL
+            response = requests.get(url, headers=header)
+            # Analizar la respuesta HTML con BeautifulSoup
+            soup = BeautifulSoup(response.content, 'html.parser')
+            # Crear un objeto DOM utilizando lxml
+            amazon_dom = et.HTML(str(soup))
 
-        product_name = get_product_name(amazon_dom)
-        product_price = get_amazon_price(amazon_dom)
+            # Extraer el nombre del producto y el precio utilizando funciones definidas anteriormente
+            product_name = get_product_name(amazon_dom)
+            product_price = get_amazon_price(amazon_dom)
 
-        time.sleep(random.randint(2, 10))
+            # Escribir los datos en el archivo CSV y mostrarlos en la consola
+            writer.writerow([product_name, product_price, url])
+            print(product_name, product_price)
 
-        writer.writerow([product_name, product_price, url])
-        print(product_name, product_price)
+    # Introducir un retraso de 1 min
+    time.sleep(60)

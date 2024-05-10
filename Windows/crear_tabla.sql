56af28f5f9a4f5b51103d5074421aa656a97d1e1
clau.v.perlado <vipeclaudia103@gmail.com>
2024-05-10 17:17:52 +0200

    docker-compose sin fastapi

--- Windows/crear_tabla.sql
+++
@@ -1,9 +0,0 @@
-CREATE SCHEMA `tiempo` ;
-CREATE TABLE `tiempo`.`temperaturas` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `Maxima` FLOAT NOT NULL,
-  `Minima` FLOAT NULL,
-  `ciudad` VARCHAR(45) NULL,
-  `latitud` FLOAT NULL,
-  `longitud` FLOAT NULL,
-  PRIMARY KEY (`id`));
